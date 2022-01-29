7aff50250037e514b5abd8636f035e4b834e06bd
DESKTOP-06IB626\alfre <alfredofweb@gmail.com>
2022-01-29 10:28:56 +0100

    Files, HTML and CSS

--- login.php
+++ login.php
@@ -5,16 +5,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
-    <title>Welcome</title>
+    <title>Login</title>
 </head>
 
 <body>
     <?php require 'partials/header.php' ?>
-    <a href="login.php">Login</a> or
-    <a href="signup.php">SignUp</a>
+    <form action="login.php" method="POST">
+        <input name="email" type="text" placeholder="Enter your email">
+        <input name="password" type="password" placeholder="Enter your Password">
+        <input type="submit" value="Submit">
+    </form>
 </body>
 
 </html>
--- signup.php
+++ signup.php
@@ -5,16 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
-    <title>Welcome</title>
+    <title>Sign Up</title>
 </head>
 
 <body>
     <?php require 'partials/header.php' ?>
-    <a href="login.php">Login</a> or
-    <a href="signup.php">SignUp</a>
+
+    <h1>Sign Up</h1>
 </body>
 
 </html>
